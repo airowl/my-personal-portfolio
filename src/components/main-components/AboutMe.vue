7092bfea766c90c9ab7e7eca2802da0a61266d16
rowl <airowl.gasga@gmail.com>
2022-05-11 22:07:11 +0200

    add: aggiunto contenuto nella section about me

--- src/components/main-components/AboutMe.vue
+++ src/components/main-components/AboutMe.vue
@@ -2,13 +2,35 @@
     <section id="about-me">
         <div class="container">
             <img src="../../assets/me.png" alt="Airowl Gasga">
+            <h1>
+                about me
+            </h1>
+            <p>
+                Mi chiamo <span>Airowl Gasga</span>, ho 23 anni e da sempre sono un appassionato di informatica e tecnologia. Sono di Nazionalità Filippina 🇵🇭  ma sono nato e cresciuto (per quasi tutta la vita) a Milano, ho conseguito il titolo di <span>Junior Full Stack Web Developer</span> presso il corso formativo di Boolean Careers. Sono sempre alla ricerca di cose nuove da imparare e cerco di tenermi aggiornato riguardo gli ultimi trend di sviluppo e tecnologie utilizzate. 
+            </p>
+            <p>
+                Le principali tecnologie che utilizzo sono queste:
+            </p>
+            <ul>
+                <li v-for="(element, index) in tech" :key="index">
+                    {{element}}
+                </li>
+            </ul>
+            <p>
+                Fuori dal lavoro, mi piace leggere manga e guardare anime giapponesi, certe volte sono anche un giocare di pallavolo come alzatore.
+            </p>
         </div>
     </section>
 </template>
 
 <script>
 export default {
-    name: 'sectionAboutMe'
+    name: 'sectionAboutMe',
+    data: function() {
+        return {
+            tech: ['html & css', 'javascript', 'vuejs', 'php', 'laravel', 'mysql']
+        }
+    }
 }
 </script>
 
@@ -20,6 +42,21 @@ section#about-me{
     img{
         width: 100%;
         border-radius: 50%;
+    }
+
+    h1, p, img{
+        margin-bottom: 1rem;
+    }
+
+    ul{
+        height: 8rem;
+        @include d-flex(column, start, start);
+        flex-wrap: wrap;
+
+        li{
+            text-transform: uppercase;
+            height: calc(100% / 3);
+        }
     }
 
 }
