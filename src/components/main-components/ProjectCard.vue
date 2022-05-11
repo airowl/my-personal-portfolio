cb41bc9780e0391bdaf978192ce4a3d30e092839
rowl <airowl.gasga@gmail.com>
2022-05-11 22:32:46 +0200

    add: aggiunto contenuto nella section projects e aggiunto nuovo component project card

---
+++ src/components/main-components/ProjectCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        elementList: Object
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../scss/style.scss';
+
+div.card{
+    width: 16rem;
+    height: 16rem;
+    background-color: rgba($font-color, $alpha: .3);
+    border-radius: 1.5rem;
+}
+
+</style>
--- src/components/main-components/Projects.vue
+++ src/components/main-components/Projects.vue
@@ -1,15 +1,74 @@
 <template>
-    <section>
-
+    <section id="projects">
+        <div class="container">
+            <h1>
+                projects
+            </h1>
+            <div class="wrapper-card">
+                <ProjectCard 
+                    v-for="(element, index) in cardList" 
+                    :key="index"
+                    :elementList="element" 
+                />
+            </div>
+        </div>
     </section>
 </template>
 
 <script>
+import ProjectCard from './ProjectCard.vue';
+
 export default {
-    name: 'sectionProjects'
+    name: 'sectionProjects',
+    components: {
+        ProjectCard
+    },
+    data: function(){
+        return {
+            cardList: [
+                {
+                    title: 'Fylo landing page',
+                    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an.',
+                    techs: [
+                        'html & css', 'vuejs'
+                    ],
+                    iconResourse: '',
+                    url: 'www.google.com'
+                },
+                {
+                    title: 'Fylo landing page',
+                    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an.',
+                    techs: [
+                        'html & css', 'vuejs'
+                    ],
+                    iconResourse: '',
+                    url: 'www.google.com'
+                },
+                {
+                    title: 'Fylo landing page',
+                    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an.',
+                    techs: [
+                        'html & css', 'vuejs'
+                    ],
+                    iconResourse: '',
+                    url: 'www.google.com'
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../../scss/style.scss';
+
+section#projects{
+
+    div.wrapper-card{
+        @include d-flex(row, start, start);
+        flex-wrap: wrap;
+        gap: 1rem;
+    }
+}
 
 </style>
