7ab81d8a4d449d81854d1996ee641bb6e27cae0d
rowl <airowl.gasga@gmail.com>
2022-05-12 20:42:12 +0200

    add: aggiunto contenuto e stilizzato del components card

--- src/components/main-components/ProjectCard.vue
+++ src/components/main-components/ProjectCard.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="card">
-        
+        <h2>{{ elementList.title }}</h2>
+        <p>{{ elementList.text }}</p>
+        <div class="bottom">
+            <span>
+                {{ elementList.techs }}
+            </span>
+            <a :href="elementList.url">
+                <i class="fa-brands fa-github"></i>
+            </a>
+        </div>
     </div>
 </template>
 
@@ -19,8 +28,29 @@ export default {
 div.card{
     width: 16rem;
     height: 16rem;
-    background-color: rgba($font-color, $alpha: .3);
+    background-color: rgba($font-color, $alpha: .2);
     border-radius: 1.5rem;
+    padding: 2rem;
+    @include d-flex(column, space-between, start);
+
+    p{
+        font-size: .6rem;
+    }
+
+    div.bottom{
+        width: 100%;
+        @include d-flex(row, space-between, center);
+
+        span{
+            font-size: .6rem;
+            color: $font-color;
+            text-transform: uppercase;
+        }
+
+        i{
+            font-size: 2rem;
+        }
+    }
 }
 
 </style>
