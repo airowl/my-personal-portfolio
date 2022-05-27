abcf2e0e41061d61bd4ccde3cd0e7336346f6783
rowl <airowl.gasga@gmail.com>
2022-05-27 22:17:38 +0200

    aggiunto npm animation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 Vue.config.productionTip = false
+AOS.init();
 
 new Vue({
   render: h => h(App),
